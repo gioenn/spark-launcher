fa2aee982bbedacb8c97965b7b48a31244bd92a4
Giovanni Quattrocchi <giovanni.quattrocchi@beqid.com>
2017-06-25 11:44:51 +0200

    generalize run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -p 4040:4040 -v $(pwd)/test.py:/test.py -v $(pwd)/dataset.txt:/dataset.txt -v $(pwd)/spark-events:/tmp/spark-events epahomov/docker-spark:lightweighted /spark/bin/spark-submit --conf "spark.eventLog.enabled=true" /test.py
+docker run -it -p 4040:4040 -v $(pwd)/$1:/$1 -v $(pwd)/$2:/$2 -v $(pwd)/spark-events:/tmp/spark-events epahomov/docker-spark:lightweighted /spark/bin/spark-submit --conf "spark.eventLog.enabled=true" /$1
